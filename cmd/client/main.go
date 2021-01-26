ee731e4ab8dbb875595418f4071c7fde41817754
charconstpointer <attwistingnether@gmail.com>
2021-01-27 00:23:52 +0100

    comms

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -9,27 +9,32 @@ import (
 
 func main() {
 
-	end, _ := kute.NewEnding()
-	middle, _ := kute.NewPipe("middle")
 	start, _ := kute.NewPipe("start")
-
+	middle, _ := kute.NewPipe("middle")
+	end, _ := kute.NewEnding()
+	//[start] -> [middle] -> [end]
 	start.Next = middle
+
 	middle.Next = end
-	end.Prev = middle
 	middle.Prev = start
+
+	end.Prev = middle
 
 	kute.RunAll(start, middle, end)
 
+	//create message
 	msg := make(kute.Header, kute.HeaderSize)
 	msg.Encode(kute.PASS, kute.HeaderSize, 1, []byte("kute"))
+
+	//send it down the pipe
 	start.Write(msg)
 	time.Sleep(time.Second)
 	b := make([]byte, 1024)
 
+	//message will have come back at this point and should be uppercase, because Ending end have modified it
 	n, err := start.Read(b)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	log.Printf("%s", b[:n])
-	time.Sleep(123312 * time.Second)
 }
